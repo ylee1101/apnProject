cdc7f22a4f1015eafafbd92182e6b6de99617213
ylee1101 <yongho19911101@hotmail.com>
2018-03-20 10:41:02 -0700

    changed format

--- App.js
+++ App.js
@@ -1,49 +1,182 @@
-import React, { Component } from "react";
-import { Button, Platform, StyleSheet, Text, View } from "react-native";
-import axios from "axios";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  AppRegistry,
+  Button
+} from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { StackNavigator } from "react-navigation";
 
-export default class App extends Component {
-  state = { data: [] };
+async function getData() {
+  const data = await fetch(
+    "https://devccc.assuredperformance.net/react_test.php"
+  );
+  return await data.json();
+}
 
-  componentWillMount() {
-    axios
-      .get("https://devccc.assuredperformance.net/react_test.php")
-      .then(response => this.setState({ data: response.data }));
+const filterFeed = array => {
+  return array.filter(element => element[0] === "drawpage");
+};
+
+const matchPage = (array1, array2) => {
+  return array1.map(page => {
+    page.elements = array2.filter(
+      element => element[1] === page[1] && element[0] === "fillpage"
+    );
+    return page;
+  });
+};
+
+const feed = [];
+// getData()
+//  .then(data => data.map(element => feed.push(element)))
+//  .then( () => console.log(feed))
+// .catch(err => alert("An error occurred"));
+
+class ScreenOne extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: false,
+      pageIndex: []
+    };
   }
 
-  // renderData() {
-  //   return this.state.data.map(data => (
-  //     <Button title={data[3]} />
-  //   ));
-  // }
+  static navigationOptions = {
+    title: "Homepage"
+  };
+
+  componentWillMount() {
+    getData()
+      .then(data => data.map(element => feed.push(element)))
+      .then(res => filterFeed(feed))
+      .then(res => matchPage(res, feed))
+      .then(res => console.log(res[0].elements, res[1].elements))
+      .then(res => this.setState({ status: true }))
+      .catch(err => alert("An error occurred"));
+  }
 
   render() {
-    console.log(this.state.data);
-    return <View style={styles.container}>
-        {/* {this.renderData()} */}
-        <Button title="Button" color="#841584" />
-        <Button title="Button" color="#841584" />
-        <Button title="Button" color="#841584" />
-        <Button title="Button" color="#841584" />
-      </View>;
+    const { navigate } = this.props.navigation;
+    if (!this.state.status) {
+      return <Text>Loading...</Text>;
+    } else {
+      return (
+        <Grid>
+          <Row>
+            <View style={styles.container}>
+              <TouchableHighlight
+                style={[styles.button, { backgroundColor: "#FECB44" }]}
+              >
+                <Text style={styles.buttonText}>{feed[2][3]}</Text>
+              </TouchableHighlight>
+            </View>
+            <View style={styles.container}>
+              <TouchableHighlight
+                style={[styles.button, { backgroundColor: "#18A25F" }]}
+              >
+                <Text style={styles.buttonText}>{feed[3][3]}</Text>
+              </TouchableHighlight>
+            </View>
+          </Row>
+          <Row>
+            <View style={styles.container}>
+              <TouchableHighlight
+                onPress={() => navigate("ScreenTwo", { screen: "Screen Two" })}
+                style={[styles.button, { backgroundColor: "#F9453B" }]}
+              >
+                <Text style={styles.buttonText}>{feed[4][3]}</Text>
+              </TouchableHighlight>
+            </View>
+            <View style={styles.container}>
+              <TouchableHighlight
+                style={[styles.button, { backgroundColor: "#4B8BF5" }]}
+              >
+                <Text style={styles.buttonText}>{feed[5][3]}</Text>
+              </TouchableHighlight>
+            </View>
+          </Row>
+        </Grid>
+      );
+    }
   }
 }
 
+class ScreenTwo extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: `${feed[1][1]}`
+    };
+  };
+  render() {
+    const { state, navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        {/* <Text style={styles.titleText}>{state.params.screen}</Text> */}
+
+        <View style={[styles.buttonContainer, { marginTop: 10 }]}>
+          <TouchableHighlight
+            onPress={() => this.props.navigation.goBack()}
+            style={[
+              styles.button,
+              { backgroundColor: "#8E84FB" },
+              { padding: 20 }
+            ]}
+          >
+            <Text style={styles.buttonText}>{feed[6][3]}</Text>
+          </TouchableHighlight>
+
+          {/* <TouchableHighlight
+            onPress={() => navigate("ScreenThree", { screen: "Screen Three" })}
+            style={[styles.button, {backgroundColor: '#8E84FB'}]}>
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableHighlight> */}
+        </View>
+      </View>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator(
+  {
+    ScreenOne: { screen: ScreenOne },
+    ScreenTwo: { screen: ScreenTwo }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#7567B1",
+      labelStyle: {
+        fontSize: 20,
+        fontWeight: "600"
+      }
+    }
+  }
+);
+
+export default SimpleApp;
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    alignItems: "stretch",
+    alignSelf: "stretch",
+    marginLeft: 5,
+    marginRight: 5
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  button: {
+    borderRadius: 10,
+    padding: 25,
+    paddingTop: 60,
+    paddingBottom: 60
   },
-  instructions: {
+  buttonText: {
+    fontSize: 18,
+    color: "#FAFAFA",
     textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+    alignSelf: "center"
   }
 });
